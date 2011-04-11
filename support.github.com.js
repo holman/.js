bbe6afd7fb3a0de5c419bb363e1797e0ccacf752
Zach Holman <github.com@zachholman.com>
2011-04-11 15:08:46 -0700

    remove the sidebar in tender

---
+++ support.github.com.js
@@ -0,0 +1,17 @@
+/*
+
+support.github.com
+
+I fix your issues on GitHub. Tender's sidebar thingymajig is silly now that
+they have the pending quickbar thingymajig.
+
+*/
+$('.cap').
+  css('background','none')
+$('#main').
+  css('background','none').
+  css('background-color','#fff')
+$('#sidebar').
+  css('display','none')
+$('#content').
+  css('width','939px')
