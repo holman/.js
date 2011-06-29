269bae61bc48543084fa49892298fd5376a58a65
Zach Holman <github.com@zachholman.com>
2011-06-28 21:26:57 -0500

    only filter the front page

--- news.ycombinator.com.js
+++ news.ycombinator.com.js
@@ -10,16 +10,18 @@ So this removes all the bullshit after the 15th story.
 
 */
 
-$('table table tbody tr:nth-child(n+45)').
-  css('display','none')
+if(window.location.pathname == '/'){
+  $('table table tbody tr:nth-child(n+45)').
+    css('display','none')
 
-$('table table:nth-child(2)').
-  append('<a id="moar" href="">More</a>')
+  $('table table:nth-child(2)').
+    append('<a id="moar" href="">More</a>')
 
-$('#moar').
-  click(function(){
-    $('table table:nth-child(1) tr').css('display','table-row')
-    $(this).hide()
-    return false;
-  }).
-  css('padding', '25px')
+  $('#moar').
+    click(function(){
+      $('table table:nth-child(1) tr').css('display','table-row')
+      $(this).hide()
+      return false;
+    }).
+    css('padding', '25px')
+}
