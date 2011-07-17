a787beacffa95ec9383d0edd97b3d2a7d670d84c
Zach Holman <github.com@zachholman.com>
2011-07-17 19:12:03 +0900

    youtube beta layout improvments

---
+++ youtube.com.js
@@ -0,0 +1,12 @@
+/*
+
+YouTube
+
+Unfettered viewing experience for the new beta YouTube layout.
+
+*/
+
+$('.watch6-feedback').hide()
+$('#watch-channel-brand-div').empty().hide()
+$('#google_companion_ad_div').empty().hide()
+$('#watch-video').delay(250).removeClass('has-ad')
