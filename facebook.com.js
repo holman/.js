3f2b17f41407fe50e6ffe06501d239d6b3bb69c5
Zach Holman <zach@zachholman.com>
2013-07-23 18:48:50 +1200

    Comment this out for now, now that I have the new fb design

--- facebook.com.js
+++ facebook.com.js
@@ -11,26 +11,26 @@ screen.
 
 */
 
-$(document).bind('DOMNodeInserted', function(event) {
-    if ($('.spotlight').attr('src') != undefined && !$('.spotlight').data('retina')) {
-      // This is sort of like an ID for the photo we're looking for. It's probably it.
-      // Something like it. Probably.
-      photo_id = $('.spotlight').attr('src').split('/').reverse()[0].split('_')[2]
-
-      // Look through all of the links on-page to see which references our image URL.
-      element  = $('a[href*=' + photo_id + ']')
-
-      // Look in that element's `ajaxify` attribute and parse out the `_o.jpg`
-      // (original) URL.
-      hd_url = unescape(element.attr('ajaxify').split('&src=')[1].split('&')[0])
-
-      // Now that we have a URL, let's embed it in the original img again.
-      $('.spotlight').attr('src', hd_url)
-
-      // Mark this as retinah'd
-      $('.spotlight').data('retina', true)
-    }
-});
+// $(document).bind('DOMNodeInserted', function(event) {
+//     if ($('.spotlight').attr('src') != undefined && !$('.spotlight').data('retina')) {
+//       // This is sort of like an ID for the photo we're looking for. It's probably it.
+//       // Something like it. Probably.
+//       photo_id = $('.spotlight').attr('src').split('/').reverse()[0].split('_')[2]
+//
+//       // Look through all of the links on-page to see which references our image URL.
+//       element  = $('a[href*=' + photo_id + ']')
+//
+//       // Look in that element's `ajaxify` attribute and parse out the `_o.jpg`
+//       // (original) URL.
+//       hd_url = unescape(element.attr('ajaxify').split('&src=')[1].split('&')[0])
+//
+//       // Now that we have a URL, let's embed it in the original img again.
+//       $('.spotlight').attr('src', hd_url)
+//
+//       // Mark this as retinah'd
+//       $('.spotlight').data('retina', true)
+//     }
+// });
 
 // I'm tired, but here we do the same swap on static, non-lightboxed photos.
 hd_download = $('a.fbPhotosPhotoActionsItem[href*=_o\\.jpg]')
